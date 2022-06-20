0c3a71904ac07dbd97414b125ff45dcb83089865
bebebio <guslgogo20@gmail.com>
2022-06-20 10:05:17 +0900

    spring07

---
+++ spring07/src/main/java/controller/WhatTimeController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class WhatTimeController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("time", new Date());
+		mav.setViewName("whatTime");
+		return mav;
+	}
+	
+}
